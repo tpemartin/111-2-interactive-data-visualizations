3bb324ca99fa7102b16a7cb01a7fc101392b2199
Martin老師 <tpemartin@gmail.com>
2023-05-05 15:59:51 +0800

    update

--- week12/week12.R
+++ week12/week12.R
@@ -3,9 +3,22 @@ ubike_data <-
     jsonlite::fromJSON()
 ubike_data
 dplyr::glimpse(ubike_data)
+
+ubikeMap = list()
 {
     library(leaflet)
-    leaflet(ubike_data) %>%
-        addTiles() %>%
-        addMarkers(~lng, ~lat)
+
+    leaflet(ubike_data) |>
+        setView(121.5288805, 25.0418781, zoom = 14) |>
+        addProviderTiles(
+            providers$CartoDB.Positron
+        ) |>
+        addCircleMarkers(
+            lng = ~lng, lat = ~lat,
+            radius = 4,
+            # color = ~pal(type),
+            stroke = FALSE, fillOpacity = 0.7
+        ) -> ubikeMap$leaflet 
 }
+
+saveRDS(ubikeMap, "ubikeMap.rds")
--- week12/week12.md
+++ week12/week12.md
@@ -1,6 +1,12 @@
 
 # leaflet
 
+## Youbike 2.0
+
+  * [新北市API](https://data.ntpc.gov.tw/openapi/swagger-ui/index.html?configUrl=%2Fapi%2Fv1%2Fopenapi%2Fswagger%2Fconfig&urls.primaryName=%E6%96%B0%E5%8C%97%E5%B8%82%E6%94%BF%E5%BA%9C%E4%BA%A4%E9%80%9A%E5%B1%80(80))
+
+  * [台北市API](https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json)
+
 ## Basemap
 
   * <https://rstudio.github.io/leaflet/basemaps.html>
