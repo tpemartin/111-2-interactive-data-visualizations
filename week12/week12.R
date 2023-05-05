71a079b9d776528e26772e23606c265a9e87d9a5
Martin老師 <tpemartin@gmail.com>
2023-05-05 17:11:35 +0800

    Update week12.R

--- week12/week12.R
+++ week12/week12.R
@@ -72,8 +72,9 @@ ubikeMap$sharedData <-
 {
   ubikeMap$dataMerged[c("lat", "lng")][500, c("lat","lng")] |>
     paste0(collapse=",") -> pin
+  currentGPS = "24.9441075,121.0692864"
   glue::glue(
-    "https://www.google.com/maps/dir/24.9441075,121.0692864/{pin}/@{pin},15z/data=!4m5!4m4!1m1!4e1!1m0!3e2"
+    "https://www.google.com/maps/dir/{currentGPS}/{pin}/@{pin},15z/data=!4m5!4m4!1m1!4e1!1m0!3e2"
   ) |>
     browseURL()
   "https://www.google.com/maps/dir/24.9441075,121.0692864/24.9456645,121.3694314/@24.9456371,121.3691364,20z/data=!4m5!4m4!1m1!4e1!1m0!3e2"
