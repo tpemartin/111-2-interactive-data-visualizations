29794c07ed067bc303cc31f657af04bbd128184b
Martin老師 <tpemartin@gmail.com>
2023-05-05 16:37:22 +0800

    update

--- week12/week12.R
+++ week12/week12.R
@@ -1,14 +1,56 @@
-ubike_data <-
-    (jsonlite::fromJSON("ubikeAPI.json"))[["data-url"]] |>
-    jsonlite::fromJSON()
-ubike_data
-dplyr::glimpse(ubike_data)
-
 ubikeMap = list()
+
+urlAPI <- jsonlite::fromJSON("ubikeAPI.json")
+
+{pages = c(0, 1)
+getNewTaipeiJSON = function(page){
+  urlAPI$`New Taipei` |>
+  paste0("&page=", page) |>
+  jsonlite::fromJSON()
+}
+# pages |>
+#   purrr::map(getNewTaipeiJSON) -> listNewTaipeiUbike
+
+pages |>
+  purrr::map_dfr(
+    getNewTaipeiJSON
+  ) -> ubikeMap$data$`New Taipei`
+
+}
+
+{ubikeMap$data$Taipei <-
+  urlAPI$Taipei |>
+  jsonlite::fromJSON()
+}
+{
+  library(magrittr)
+  ubikeMap$data$Taipei %<>%
+    dplyr::mutate(
+      act = as.integer(act)
+    )
+  ubikeMap$data$`New Taipei` %<>%
+    dplyr::mutate(
+      tot = as.integer(tot),
+      sbi = as.integer(sbi),
+      lat = as.double(lat),
+      lng = as.double(lng),
+      bemp = as.integer(bemp),
+      act = as.integer(act)
+    )
+  dplyr::glimpse(ubikeMap$data$Taipei)
+}
+dplyr::bind_rows(
+  ubikeMap$data$Taipei,
+  ubikeMap$data$`New Taipei`
+) -> ubikeMap$dataMerged
+
+ubikeMap$sharedData <-
+  crosstalk::SharedData$new(ubikeMap$dataMerged)
+
 {
     library(leaflet)
 
-    leaflet(ubike_data) |>
+    leaflet(ubikeMap$sharedData) |>
         setView(121.5288805, 25.0418781, zoom = 14) |>
         addProviderTiles(
             providers$CartoDB.Positron
@@ -18,7 +60,11 @@ ubikeMap = list()
             radius = 4,
             # color = ~pal(type),
             stroke = FALSE, fillOpacity = 0.7
-        ) -> ubikeMap$leaflet 
+        ) |>
+      addEasyButton(easyButton(
+        icon="fa-crosshairs", title="Locate Me",
+        onClick=JS("function(btn, map){ map.locate({setView: true}); }")))-> ubikeMap$leaflet
+
 }
 
 saveRDS(ubikeMap, "ubikeMap.rds")
--- week12/week12.md
+++ week12/week12.md
@@ -4,6 +4,7 @@
 ## Youbike 2.0
 
   * [新北市API](https://data.ntpc.gov.tw/openapi/swagger-ui/index.html?configUrl=%2Fapi%2Fv1%2Fopenapi%2Fswagger%2Fconfig&urls.primaryName=%E6%96%B0%E5%8C%97%E5%B8%82%E6%94%BF%E5%BA%9C%E4%BA%A4%E9%80%9A%E5%B1%80(80))
+    * Youbike2.0 500 obs: <https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?size=500>
 
   * [台北市API](https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json)
 
