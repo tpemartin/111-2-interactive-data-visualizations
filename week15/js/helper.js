2d9492bfe22915989e1f1055066e7d6de680e311
Martin老師 <tpemartin@gmail.com>
2023-06-02 17:13:25 +0800

    update

--- week15/js/helper.js
+++ week15/js/helper.js
@@ -15,8 +15,8 @@ function prepareBaseMap(currentLocation) {
 
       // Use CartoDB.positron
       var CartoDB_Positron = L.tileLayer(
-        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', 
-        {"errorTileUrl":"","noWrap":false,"detectRetina":false}) 
+        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+        {"errorTileUrl":"","noWrap":false,"detectRetina":false})
       CartoDB_Positron.addTo(leaflet.map);
 
 //       // Use Esri.NatGeoWorldMap
@@ -25,14 +25,14 @@ function prepareBaseMap(currentLocation) {
 // 	maxZoom: 16
 // });
 //       Esri_NatGeoWorldMap.addTo(leaflet.map);
-      
-     
+
+
   } else {
     leaflet.map.setView(currentLocation, 13);
   }
-  
 
-  
+
+
   // Add marker
   leaflet.currentLocationMarker =
     L.marker(currentLocation).addTo(leaflet.map);
@@ -47,7 +47,13 @@ function successCallback(position) {
     {lat: currentLocation[0], lng: currentLocation[1]}
   )
   prepareBaseMap(currentLocation);
-  
+  // addCircleMarkers
+  args={"lat":25.05,"lng":121.5,"radius":4,"markerOptions":{"radius":4,"interactive":true,"className":"","stroke":false,"color":"#03F","weight":5,"opacity":0.5,"fill":true,"fillColor":"#03F","fillOpacity":0.7},"popupContent":"<a href=\"https://www.google.com/maps/@{lat},{lng},15z\">click me<\/a>","popupOptions":{"interactive":false,"permanent":false,"direction":"auto","opacity":1,"offset":[0,0],"textsize":"10px","textOnly":false,"className":"","sticky":true}}
+  leaflet.min5.forEach(
+      addStation
+  )
+
+
 };
 
 function errorCallback(error) {
@@ -82,4 +88,28 @@ function findCloset5StationFromCurrentLocation(currentLocation){
     }
 
 leaflet.min5 = min5
+}
+function addStation(station){
+    direction = getDirection(station)
+    popupContent = getPopupContent(direction)
+    latLng = L.latLng(station)
+    mk = L.circleMarker(latLng, args.markerOptions)
+    mk.bindPopup(
+        popupContent,args.popupOptions)
+    mk.addTo(leaflet.map)
+}
+
+// direction
+// https://www.google.com/maps/dir/{destination}/@{currentLocation},17z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x34681c0a444c22c3:0x7c9771cca8fa4362!2m2!1d121.37326!2d24.945553!3e1?entry=ttu
+function getDirection(station){
+    currentMarker = leaflet.currentLocationMarker._latlng
+    currentLocation = currentMarker.lat +','+currentMarker.lng
+    destination = station.lat+','+station.lng
+    direction = `https://www.google.com/maps/dir/${destination}/@${currentLocation},17z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x34681c0a444c22c3:0x7c9771cca8fa4362!2m2!1d121.37326!2d24.945553!3e1?entry=ttu`
+  return(direction)
+}
+
+function getPopupContent(direction){
+       popupContent =  `<a href="${direction}">click me</a>`
+    return(popupContent)
 }
